5987a6b5c7a791f93116e2ab882b44e674c2c15a
atercattus <cat@ater.me>
2021-09-13 14:43:33 +0300

    initialize

---
+++ cmd/lession01/homework/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("\033[31mHello \033[0mWorld") // https://www.shellhacks.com/bash-colors/
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tfs-go/lections21
+
+go 1.16
